445ae4b1e4f1c6ef9cfe11aa046da8693654287c
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-08-23 10:00:43 -0500

    Parametros por defecto en funciones

---
+++ src/es6/04-default-params.js
@@ -0,0 +1,18 @@
+// forma normal
+function newUser(name, age, country) {
+  var name = name || "jakepys";
+  var age = age || 19;
+  var country = country || "CO";
+  console.log(name, age, country);
+}
+
+newUser();
+newUser("noe", 59);
+
+// forma es6
+function newAdmin(name = "admin", password = "root", role = "Developer") {
+  console.log(name, password, role);
+}
+
+newAdmin();
+newAdmin("jakepy", "passwordofjakepy", "Developer Js DevOps");
