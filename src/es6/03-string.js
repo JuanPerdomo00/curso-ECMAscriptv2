67cf04f3b64bd1e705d9bc5308661e787fba3fc2
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-08-23 09:44:52 -0500

    Strings, template literal, milti line

---
+++ src/es6/03-string.js
@@ -0,0 +1,19 @@
+// concatenar normal
+let hello = "Hello";
+let world = "World";
+let simbol = "!";
+
+let completeFrase = hello + " " + world + simbol;
+// console.log(completeFrase);
+
+// template literal
+let completeFrase2 = `${hello} ${world}${simbol}`;
+console.log(completeFrase2);
+
+// Multinea
+let lorem =
+  "Lorem ipsum dolor sit amet, consectetur \n" +
+  " Lorem ipsum dolor sit amet, consectetur";
+
+let lorem2 = `lorem ipsum dolor sit amet, consectetur
+caminante sigue no te detengas adelante`;
