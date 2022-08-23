f0c84d19c816511a21ac6b5dc131a5d1f442a11c
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-08-23 09:28:56 -0500

    Funciones de flecha, y el return inplicito

---
+++ src/es6/01-arrow-function.js
@@ -0,0 +1,16 @@
+// Funcion Normal
+function raiz(num) {
+  return num ** (1 / 2);
+}
+
+// funcion flrcha con return
+const raiz = (num) => {
+  return num ** (1 / 2);
+};
+
+// funcion flecha sin return
+// por lo general se usa cuando es un solo parametro
+
+const raiz = (num) => num ** (1 / 2);
+
+console.log(raiz(2));
