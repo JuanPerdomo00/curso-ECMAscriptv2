3ad5263227a03ba784aca5c3d6a88fed91b06e00
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-08-23 09:01:22 -0500

    global scope y local scope var, let, const

---
+++ src/es6/00-let-const.js
@@ -0,0 +1,24 @@
+var lastName = "Jakepy"; // Declarar y asignar
+lastName = "Juan"; // ree asignar
+console.log(lastName);
+
+let fruit = "apple";
+fruit = "orange";
+console.log(fruit);
+
+const animal = "Dog";
+animal = "Cat";
+console.log(animal);
+
+const frutas = () => {
+  if (true) {
+    var fruit1 = "apple";
+    let fruit2 = "orange";
+    const fruit3 = "banana";
+  }
+  console.log(fruit1);
+  console.log(fruit2);
+  console.log(fruit3);
+};
+
+frutas();
