06ea1a897c33ca719190aba0dc627346910eed5c
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-09-01 09:22:01 -0500

    se modifico

--- src/es6/04-default-params.js
+++ src/es6/04-default-params.js
@@ -1,4 +1,4 @@
-// forma normal
+//! forma normal
 function newUser(name, age, country) {
   var name = name || "jakepys";
   var age = age || 19;
@@ -9,7 +9,7 @@ function newUser(name, age, country) {
 newUser();
 newUser("noe", 59);
 
-// forma es6
+//! forma es6
 function newAdmin(name = "admin", password = "root", role = "Developer") {
   console.log(name, password, role);
 }
