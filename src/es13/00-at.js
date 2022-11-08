70f28a4640f07e19d9f541ce1ed06488a311937e
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-11-08 07:49:31 -0500

    especificacion es13

---
+++ src/es13/00-at.js
@@ -0,0 +1,15 @@
+const arr = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "ten",
+];
+
+console.log(arr[arr.length - 1]);
+console.log(arr.at(-1));
