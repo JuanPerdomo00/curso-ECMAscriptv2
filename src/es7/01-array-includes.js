116973e1643e86a0c64286dcdeaa51b0319fec52
Juan Perdomo - Jakepy <perdomojuan187@gmail.com>
2022-11-08 05:50:55 -0500

    include para saber si existe el elemento

---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,12 @@
+/* 
+! Includes
+*/
+
+let array = [1, 2, 3, 4, 5];
+
+// preguntar si el item existe
+console.log(array.includes(4));
+
+// preguntar si existe un nombre
+let listName = ["jakepy", "victor", "papa", "pico"];
+console.log(listName.includes("Jakepy".toLowerCase()));
